01756ae2cf3166ac2c0154c6989be9c19dbed239
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-04-09 12:09:26 -0500

    finished the multiplication table for forloop

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,7 +1,14 @@
 "use strict";
 
 (function () {
+var b = 1;
+function showMultiplicationTable(a){
+    do {
+        console.log(a + " x " + b + " = " + (a * b));
+        b++
+    } while (b <= 10)
+}
 
-
+console.log(showMultiplicationTable(7))
 
 })();

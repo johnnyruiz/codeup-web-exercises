58499d597df3cba7a28009b8f0a148adb8229613
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-04-09 13:51:53 -0500

    saticfied the exercises for loops

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -9,6 +9,25 @@ function showMultiplicationTable(a){
     } while (b <= 10)
 }
 
-console.log(showMultiplicationTable(7))
+console.log(showMultiplicationTable(7));
+
+    for(var x = 100; x >= 5; x -= 5){
+        console.log(x);
+    }
+
+    function isEven(a){
+        if (a % 2 === 0){
+            console.log(a + " is even")
+        } else {
+            console.log(a + " is odd")
+        } return a;
+    }
+    for(var z = 1; z <= 10; z++){
+        console.log(isEven(Math.floor(Math.random() * 50) + 50));
+    }
+
+    for(var j = 1; j < 10; j++){
+        console.log(j.toString().repeat(j));
+    }
 
 })();

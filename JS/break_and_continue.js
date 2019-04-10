6c1c7abda80dc30b632d3829fab49a81d2352015
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-04-10 13:32:22 -0500

    revised some solutions to loop exercises

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -2,14 +2,11 @@
 
 (function () {
 
-
     do{
         var number = prompt("Input an ODD number between 1 and 50");
         if(number % 2 != 0){
             break;
         }
     } while (number <= 50);
-
-
 
 })();
--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,36 +1,41 @@
 "use strict";
 
 (function () {
+
+
     var b = 1;
+
     do {
         var userInput = prompt("Enter a number for a multiplication chart");
     } while (isNaN(+userInput));
 
-function showMultiplicationTable(a){
-    do {
-        console.log(a + " x " + b + " = " + (a * b));
-        b++
-    } while (b <= 10)
-}
+    function showMultiplicationTable(a) {
+        do {
+            console.log(a + " x " + b + " = " + (a * b));
+            b++
+        } while (b <= 10)
+    }
 
-console.log(showMultiplicationTable(userInput));
+    console.log(showMultiplicationTable(userInput));
 
-    for(var x = 100; x >= 5; x -= 5){
+    for (var x = 100; x >= 5; x -= 5) {
         console.log(x);
     }
 
-    function isEven(a){
-        if (a % 2 === 0){
+    function isEven(a) {
+        if (a % 2 === 0) {
             console.log(a + " is even")
         } else {
             console.log(a + " is odd")
-        } return a;
+        }
+        return a;
     }
-    for(var z = 1; z <= 10; z++){
+
+    for (var z = 1; z <= 10; z++) {
         console.log(isEven(Math.floor(Math.random() * 50) + 50));
     }
 
-    for(var j = 1; j < 10; j++){
+    for (var j = 1; j < 10; j++){
         console.log(j.toString().repeat(j));
     }
 

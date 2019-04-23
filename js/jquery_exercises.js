8af6430e2eaebc896c73c8c6d5dada3652c48a0e
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-04-23 10:46:32 -0500

    selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,7 +1,40 @@
 "use strict";
+/////////////  NOTES  /////////////////////
+//***************************************//
+//selects all the h1 elements on the page
+//.html("") will change the inner html
+//$("h1").html("")
+//.css("property", "value") to change the css
+//$("#id") to select ID
+//$(".class") to select class
+//$("*")
+//$("selector", "another selector") to select multiple selectors
+//$("h1:nth-child(3)"). will select the 3rd h1 in the document
 
-$(document).ready(function() {
 
-    // INSERT JAVASCRIPT CODE SAMPLE HERE
+//will fire off on load
+//$(document).ready(function() {
+//    alert( 'The DOM has finished loading!' );
+//    // INSERT JAVASCRIPT CODE SAMPLE HERE
+//
+//});
+
+//$("#sameId").html("<h5> Now I'm an h5 </h5>");
+//
+//$(".city").css("border", "solid 1px red");
+
+$("#falling").click(function(){
+    $("img").slideUp();
+
 
 });
+$("#showing").click(function(){
+    $("img").slideDown();
+});
+
+$("li").css("font-size", "20px");
+
+$("p").css("background-color", "yellow");
+
+$("#bottomContent").append("<h1>Fading out.......</h1>").fadeOut(6000);
+

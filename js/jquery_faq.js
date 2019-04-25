703431d584e9a31d5a64cf5372d405877d4855fa
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-04-25 10:00:50 -0500

    need to fix functions for buttons

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -8,3 +8,52 @@ $('#show').click(function(){
 $('dt').click(function(){
    $(this).addClass('highlight');
 });
+$('#highlight').click(function(){
+    $('li').toggleClass('highlight');
+});
+$('h3').click(function(){
+   $('ul').children('li').css('font-weight', 'bold');
+});
+$('li').click(function(){
+   $(this).parent('ul').children('li').eq(0).css('color', 'blue');
+});
+
+$('#YB').click(function(){
+    if($(this).parent('div').hasClass('yellowstone')){
+        $(this).parent('div').removeClass('yellowstone').addClass('glacier');
+        $('#GB').parent('div').removeClass('glacier').addClass('yellowstone');
+    } else if ($(this).parent('div').hasClass('glacier')){
+        $(this).parent('div').removeClass('glacier').addClass('yellowstone');
+        $('#GB').parent('div').removeClass('yellowstone').addClass('glacier');
+    } else if ($(this).parent('div').hasClass('grand')){
+        $(this).parent('div').removeClass('grand').addClass('yellowstone');
+        $('#GB').parent('div').removeClass('yellowstone').addClass('grand');
+    }
+});
+$('#GCB').click(function(){
+    if($(this).parent('div').hasClass('yellowstone')){
+        $(this).parent('div').removeClass('yellowstone').addClass('glacier');
+        $('#GB').parent('div').removeClass('glacier').addClass('yellowstone');
+    } else if ($(this).parent('div').hasClass('glacier')){
+        $(this).parent('div').removeClass('glacier').addClass('yellowstone');
+        $('#GB').parent('div').removeClass('yellowstone').addClass('glacier');
+    } else if ($(this).parent('div').hasClass('grand')){
+        $(this).parent('div').removeClass('grand').addClass('yellowstone');
+        $('#GB').parent('div').removeClass('yellowstone').addClass('grand');
+    }
+});
+$('#GB').click(function(){
+    if($(this).parent('div').hasClass('yellowstone')){
+        $(this).parent('div').removeClass('yellowstone').addClass('glacier');
+        $('#GB').parent('div').removeClass('glacier').addClass('yellowstone');
+    } else if ($(this).parent('div').hasClass('glacier')){
+        $(this).parent('div').removeClass('glacier').addClass('yellowstone');
+        $('#GB').parent('div').removeClass('yellowstone').addClass('glacier');
+    } else if ($(this).parent('div').hasClass('grand')){
+        $(this).parent('div').removeClass('grand').addClass('yellowstone');
+        $('#GB').parent('div').removeClass('yellowstone').addClass('grand');
+    }
+});
+
+
+
